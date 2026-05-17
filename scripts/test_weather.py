22281ce5509ed23749a934203d5a42d26fde77fb
liamli <1625677119@qq.com>
2026-05-17 09:48:12 +0800

    添加日志信息

--- scripts/test_weather.py
+++ scripts/test_weather.py
@@ -16,6 +16,7 @@ class TestWeather:
             # 调用天气请求接口传入参数
             result = WeatherApi.weather_api(param = param)
             print(result)
+            logger.info(f"接口响应结果 {result}")
         except Exception as e:
             print(e)
             logger.info(e)
